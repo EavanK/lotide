ab0ad4c9bba0a9633eea996c01bd58ce3b5a06b0
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-01 20:18:23 +0000

    refactored, modularized

--- head.js
+++ head.js
@@ -1,20 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = (array) => {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-// An array with only one element should still yield that one element as its head
-assertEqual(head([5]), 5);
-
-//An empty array should yield undefined as its head
-assertEqual(head([]), undefined);
+module.exports = head;
