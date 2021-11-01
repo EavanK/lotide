4f6d21f19852614d9f2d2dd0e538eb9cfe33e070
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-01 04:54:59 +0000

    assertArraysEqual function refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,11 +16,12 @@ const eqArrays = (arr1, arr2) => {
 
 
 const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`👍👍👍 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`😡😡😡 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
+
+  console.log((eqArrays(arr1, arr2)) ?
+
+    `👍👍👍 Assertion Passed: ${arr1} === ${arr2}` :
+
+    `😡😡😡 Assertion Failed: ${arr1} !== ${arr2}`);
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
