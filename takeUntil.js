4c80b3bf573b9257db93e7a5891674938d2342ef
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-01 05:57:35 +0000

    refactored eqArrays, assertArraysEqual functions

--- takeUntil.js
+++ takeUntil.js
@@ -1,24 +1,26 @@
 const eqArrays = (arr1, arr2) => {
+
   let result = false;
+
   if (arr1.length === arr2.length) {
+
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        result = false;
-        break;
-      } else {
-        result = true;
-      }
+
+      if (arr1[i] === arr2[i]) result = true;
+      
+      else return false;
     }
   }
   return result;
 };
 
 const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`👍👍👍 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`😡😡😡 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
+
+  console.log((eqArrays(arr1, arr2)) ?
+
+    `👍👍👍 Assertion Passed: ${arr1} === ${arr2}` :
+
+    `😡😡😡 Assertion Failed: ${arr1} !== ${arr2}`);
 };
 
 const takeUntil = (array, callback) => {
