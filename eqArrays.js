788bca58b97b82d671d91e63ffc7ba0508c33daf
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-01 20:19:16 +0000

    refactored modularized

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  console.log((actual === expected) ?
-  
-    `👍👍👍 Assertion Passed: ${actual} === ${expected}` :
- 
-    `😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
-  
-};
-
 const eqArrays = (arr1, arr2) => {
 
   let result = false;
@@ -25,12 +15,4 @@ const eqArrays = (arr1, arr2) => {
 };
 
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
-assertEqual(eqArrays([1, '2', 3], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 3], ['1', 2, 3]), false);
+module.exports = eqArrays;
