ccb30053658aa507807c07f9d4306d3c209959f5
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-01 05:14:05 +0000

    refactored functions

--- middle.js
+++ middle.js
@@ -1,27 +1,33 @@
 const assertArraysEqual = (arr1, arr2) => {
 
-  if (eqArrays(arr1, arr2)) {
-    
-    console.log(`👍👍👍 Assertion Passed: ${arr1} === ${arr2}`);
-  
-  } else {
+  console.log((eqArrays(arr1, arr2)) ?
 
-    console.log(`😡😡😡 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
+    `👍👍👍 Assertion Passed: ${arr1} === ${arr2}` :
+
+    `😡😡😡 Assertion Failed: ${arr1} !== ${arr2}`);
 };
 
 const eqArrays = (arr1, arr2) => {
 
-  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
+  let result = false;
 
-    return true;
+  if (arr1.length === arr2.length) {
 
-  } else return false;
+    for (let i = 0; i < arr1.length; i++) {
+
+      if (arr1[i] === arr2[i]) result = true;
+      
+      else return false;
+    }
+  }
+  return result;
 };
 
-const middle = (arr) => {
+
+const middle = arr => {
 
   const result = [];
+
   let i = arr.length - 1;
 
   if (i !== 0 && i !== 1) {
@@ -40,11 +46,11 @@ const middle = (arr) => {
   return result;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1]), []); // false
+assertArraysEqual(middle([1, 2]), []); // false
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5]); // false
