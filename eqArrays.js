0cc6859094a9aea31b72681506e93d13dca9b749
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-01 04:58:10 +0000

    assertEqual refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,11 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
-  }
+
+  console.log((actual === expected) ?
+  
+    `👍👍👍 Assertion Passed: ${actual} === ${expected}` :
+ 
+    `😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  
 };
 
 const eqArrays = (arr1, arr2) => {
