23840005dc3d309c0ba9c2ae3bc31d5fa6a68a8a
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-02 19:44:45 +0000

    updated Object Shorthand

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const flatten = require('./flatten');
+const map = require('./map');
+const without = require('./without');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const takeUntil = require('./takeUntil');
 
-module.exports = {head, tail, middle};
+
+
+module.exports = {head, tail, middle, flatten, map, without, letterPositions, findKeyByValue, countOnly, eqArrays, eqObjects, takeUntil};
